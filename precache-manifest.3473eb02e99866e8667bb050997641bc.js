f9fb15697512c7c70a0a635e8ff11f1191ec0c21
Nico Barbarese <nicobarbarese@gmail.com>
2019-10-03 18:56:28 -0400

    Updates

---
+++ precache-manifest.3473eb02e99866e8667bb050997641bc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b7b57b841e863406269f60540de35c43",
+    "url": "/clicky-game/index.html"
+  },
+  {
+    "revision": "69e12f8ed909b3a3332a",
+    "url": "/clicky-game/static/css/main.054bebe5.chunk.css"
+  },
+  {
+    "revision": "0b831a15654f4b758178",
+    "url": "/clicky-game/static/js/2.11f40f6e.chunk.js"
+  },
+  {
+    "revision": "69e12f8ed909b3a3332a",
+    "url": "/clicky-game/static/js/main.3de57d82.chunk.js"
+  },
+  {
+    "revision": "0826f9c9377742a6e5c7",
+    "url": "/clicky-game/static/js/runtime-main.2c299f57.js"
+  }
+]);
